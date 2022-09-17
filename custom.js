561cf8d77f15012208363a9b25ddc23ade308cac
Nevermore98 <39004291+Nevermore98@users.noreply.github.com>
2022-09-17 15:55:07 +0000

    Deploying to gh-pages from @ Nevermore98/Nevermore98.github.io@d0072badbb61097b54686960966f2cef485981a8 🚀

--- custom.js
+++ custom.js
@@ -1,15 +1,19 @@
 // 想隐藏手机端在首页时的侧边栏（目前只会这种笨方法）window.onload 不成功（window.onload 只能存在一个且执行一次）
 setTimeout(() => {
-  let toggleButton = document.querySelector('.toggle-sidebar-button')
-  // 判断是否在首页
-  if (
-    window.location.href === 'http://localhost:8080/' ||
-    window.location.href === 'http://monkee.online/' ||
-    window.location.href === 'https://monkee.online/' ||
-    window.location.href === 'https://nevermore98.github.io/' ||
-    window.location.href === 'https://nevermore98.vercel.app/'
-  ) {
-    // toggleButton.style.setProperty('display', 'none', 'important')
-    toggleButton.style.display = 'none'
+  try {
+    let toggleButton = document.querySelector('.toggle-sidebar-button')
+    // 判断是否在首页
+    if (
+      window.location.href === 'http://localhost:8080/' ||
+      window.location.href === 'http://monkee.online/' ||
+      window.location.href === 'https://monkee.online/' ||
+      window.location.href === 'https://nevermore98.github.io/' ||
+      window.location.href === 'https://nevermore98.vercel.app/'
+    ) {
+      // toggleButton.style.setProperty('display', 'none', 'important')
+      toggleButton.style.display = 'none'
+    }
+  } catch (error) {
+    // console.log(error)
   }
 }, 50)
