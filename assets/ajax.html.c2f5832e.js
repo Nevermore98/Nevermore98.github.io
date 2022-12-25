9f6cb06328a114598eceb04360d240c9f7acbc83
Nevermore98 <39004291+Nevermore98@users.noreply.github.com>
2022-12-25 07:19:00 +0000

    Deploying to gh-pages from @ Nevermore98/Nevermore98.github.io@e25f24a6d78207264d25061bec4f3b0a8aadcee9 🚀

---
+++ assets/ajax.html.c2f5832e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as r,c as a}from"./app.87656235.js";const t={};function c(o,_){return r(),a("div")}var m=e(t,[["render",c],["__file","ajax.html.vue"]]);export{m as default};
